023bc39209ad0e794a4e79b6cf239bd68d3ca7f8
Paul Reioux <reioux@gmail.com>
2013-05-19 02:52:57 -0500

    krait-defines: create a new common header file shared used by various drivers
    code to help organize common MACRO definitions into 1 file
    
    Signed-off-by: Paul Reioux <reioux@gmail.com>

--- arch/arm/mach-msm/acpuclock-krait.c
+++ arch/arm/mach-msm/acpuclock-krait.c
@@ -43,11 +43,7 @@
 #include <mach/sec_debug.h>
 #endif
 
-#ifdef CONFIG_LOW_CPUCLOCKS
-#define FREQ_TABLE_SIZE		39
-#else
-#define FREQ_TABLE_SIZE		35
-#endif
+#include "krait-defines.h"
 
 /* MUX source selects. */
 #define PRI_SRC_SEL_SEC_SRC	0
